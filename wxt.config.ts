aa87ea3296b8464bffc117856eb9292333907ef9
chengxilo <chengxi.luo2004@gmail.com>
2025-05-31 10:50:44 -0400

    docs: add the link to Chrome Web Store.

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,9 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
+    name: "Better CUNY",
+    description: "Provide additional features for the CUNY website",
+    version: "0.0.1",
     permissions: ["storage"],
     host_permissions: ["https://sb.cunyfirst.cuny.edu/*"]
   }
